cc7015d57986530ffe08512668ece5ffefb9caec
Thineskumar Thilakanathan <thineskumarthilakanathan@Thineskumars-MacBook-Pro.local>
2019-09-14 22:19:59 +0200

    Added google fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #eee;
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1,h2,h3,h4,h5 {
+    font-family: 'Great Vibes', cursive;
+  }
+`;
 
 function App() {
-  return <div>Kaviyazhan</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        <h1>Kaviyazhan</h1>
+        <p>Tamil Lyricist</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
